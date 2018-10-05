9b45226fb99e470ada361f27f0fa6793b3f45020
StandardGiraffe <dannyfekete@gmail.com>
2018-10-05 19:13:43 +0000

    Added methods to change/get rating from tracks.

--- library.js
+++ library.js
@@ -1,3 +1,12 @@
+// #################
+// Helper functions
+// #################
+
+
+
+// ########
+// Classes
+// ########
 class Library {
   constructor(name, creator) {
     this.name = name;
@@ -20,8 +29,33 @@ class Playlist {
 
 class Track {
   constructor(title, rating, length) {
+    let validatedInitialRating = this.validateRating(rating);
+
     this.title = title;
-    this.rating = rating;
+    this._rating = this.validateRating(rating);
     this.length = length;
   }
+
+  validateRating(rating) {
+    if (rating >= 0 && rating <= 5) {
+      return rating;
+    } else {
+      console.log(`Chosen rating of ${rating} is not between 0 and 5.  Rating has been set to 0.`);
+      return 0;
+    }
+  }
+
+  get rating() {
+    return this._rating;
+  }
+
+  setRating(rating) {
+    this._rating = this.validateRating(rating);
+    console.log(`${this.title} is now rated ${this._rating} out of 5.`);
+  }
 }
+
+// #############
+// Test-driving:
+// #############
+const myTrack = new Track("No No Raja", 4, 201);
